2f639b580ca1b937602389f9c22ab8bdb9cab213
rachmadaniHaryono <foreturiga@gmail.com>
2018-11-15 04:43:20 +0800

    new: dev: flask script init

--- script.py
+++ script.py
@@ -15,9 +15,18 @@ required pkg:
 import hashlib
 import shutil
 import tempfile
+import os
+from datetime import datetime
 
-from mitmproxy.script import concurrent
+from flask import Flask
+from flask.cli import FlaskGroup
+from flask_sqlalchemy import SQLAlchemy
 from mitmproxy import ctx, http
+from mitmproxy.script import concurrent
+from pillow import Image  # NOQA
+from sqlalchemy.types import TIMESTAMP
+from sqlalchemy_utils.types import URLType
+import click
 
 
 def chunks(l, n):
@@ -62,3 +71,60 @@ def response(flow: http.HTTPFlow) -> None:
                     req_url = flow.request.pretty_url  # NOQA
                     #  sha256_csum
                     #  ext
+
+
+# MODEL
+db = SQLAlchemy()
+url_tags = db.Table(
+    'url_tags',
+    db.Column('url_id', db.Integer, db.ForeignKey('url.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True))
+
+
+class Base(db.Model):
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(TIMESTAMP, default=datetime.now, nullable=False)
+
+
+class Sha256Checksum(Base):
+    value = db.Column(db.String, unique=True)
+    ext = db.Column(db.String)
+    filesize = db.Column(db.Integer)
+    height = db.Column(db.Integer)
+    width = db.Column(db.Integer)
+    img_format = db.Column(db.String)
+    img_mode = db.Column(db.String)
+    urls = db.relationship('Url', lazy='subquery', backref='checksum')
+    trash = db.Column(db.Boolean, default=False)
+
+
+class Url(Base):
+    value = db.Column(URLType, unique=True, nullable=False)
+
+
+# FLASK
+def create_app(script_info=None):
+    """create app."""
+    app = Flask(__name__)
+    database_path = 'mitmproxy_image.db'
+    database_uri = 'sqlite:///' + database_path
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_uri # NOQA
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.urandom(24)
+    app.config['WTF_CSRF_ENABLED'] = False
+
+    # app and db
+    db.init_app(app)
+    app.app_context().push()
+    db.create_all()
+
+
+@click.group(cls=FlaskGroup, create_app=create_app)
+def cli():
+    """This is a script for application."""
+    pass
+
+
+if __name__ == '__main__':
+    cli()
