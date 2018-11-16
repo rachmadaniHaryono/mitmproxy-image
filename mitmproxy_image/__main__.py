38ff3d4d623cb269535a1a6238f24f3eabad1e2c
rachmadaniHaryono <foreturiga@gmail.com>
2018-11-16 11:20:07 +0800

    chg: dev: main script
    - remove package list to setup.py
    - renamed func to write_file
    - remove unused url tags
    - fix flask app
    - add command to echo path file
    - fix pillow import
    - not using concurrent on default

--- mitmproxy_image/__main__.py
+++ mitmproxy_image/__main__.py
@@ -1,16 +1,10 @@
+#!/usr/bin/env python
 """This script download all the image.
 
 reference:
 https://github.com/mitmproxy/mitmproxy/blob/master/examples/simple/internet_in_mirror.py
 https://gist.github.com/denschub/2fcc4e03a11039616e5e6e599666f952
 https://stackoverflow.com/a/44873382/1766261
-
-dev pkg:
-- flake8==3.6.0
-- pdbpp==0.9.2
-
-required pkg:
-- mitmproxy==4.0.4
 """
 import hashlib
 import shutil
@@ -23,7 +17,7 @@ from flask.cli import FlaskGroup
 from flask_sqlalchemy import SQLAlchemy
 from mitmproxy import ctx, http
 from mitmproxy.script import concurrent
-from pillow import Image  # NOQA
+from PIL import Image  # NOQA
 from sqlalchemy.types import TIMESTAMP
 from sqlalchemy_utils.types import URLType
 import click
@@ -35,7 +29,7 @@ def chunks(l, n):
         yield l[i:i + n]
 
 
-def write_file_async(flow_item, ext):
+def write_file(flow_item, ext):
     h = hashlib.sha256()
     block = 128*1024
     try:
@@ -54,7 +48,6 @@ def write_file_async(flow_item, ext):
         ctx.log.error('{}:{}'.format(type(e), e))
 
 
-@concurrent
 def response(flow: http.HTTPFlow) -> None:
     if 'content-type' in flow.response.headers:
         content_type = flow.response.headers['content-type']
@@ -65,7 +58,7 @@ def response(flow: http.HTTPFlow) -> None:
                 ext = content_type.split('/')[1].split(';')[0]
                 invalid_exts = ['svg+xml', 'x-icon', 'gif']
                 if ext not in invalid_exts:
-                    write_file_async(flow, ext)
+                    write_file(flow, ext)
 
                     # send to server
                     req_url = flow.request.pretty_url  # NOQA
@@ -75,10 +68,6 @@ def response(flow: http.HTTPFlow) -> None:
 
 # MODEL
 db = SQLAlchemy()
-url_tags = db.Table(
-    'url_tags',
-    db.Column('url_id', db.Integer, db.ForeignKey('url.id'), primary_key=True),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True))
 
 
 class Base(db.Model):
@@ -118,12 +107,19 @@ def create_app(script_info=None):
     db.init_app(app)
     app.app_context().push()
     db.create_all()
+    return app
 
 
 @click.group(cls=FlaskGroup, create_app=create_app)
 def cli():
     """This is a script for application."""
     pass
+
+
+@cli.command('echo-path')
+def echo_path():
+    'Echo path to script.'
+    print(__file__)
 
 
 if __name__ == '__main__':
