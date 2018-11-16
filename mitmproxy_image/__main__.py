206f08324138a00767772d4f1e8370ea48351415
rachmadaniHaryono <foreturiga@gmail.com>
2018-11-16 14:21:59 +0800

    new: dev: db sync !wip
    - fix typo on shell_context_processor
    - change mitmproxy add on to class style
    - add method to get more  info from flow obj

--- mitmproxy_image/__main__.py
+++ mitmproxy_image/__main__.py
@@ -7,6 +7,7 @@ https://gist.github.com/denschub/2fcc4e03a11039616e5e6e599666f952
 https://stackoverflow.com/a/44873382/1766261
 """
 from datetime import datetime
+from io import BytesIO
 import hashlib
 import os
 import pathlib  # require python 3.5+
@@ -17,7 +18,7 @@ from flask import Flask
 from flask.cli import FlaskGroup
 from flask_sqlalchemy import SQLAlchemy
 from mitmproxy import ctx, http
-from PIL import Image  # NOQA
+from PIL import Image
 from sqlalchemy.types import TIMESTAMP
 from sqlalchemy_utils.types import URLType
 import click
@@ -29,10 +30,12 @@ def chunks(l, n):
         yield l[i:i + n]
 
 
-def write_file(flow_item, ext):
+def process_flow(flow_item, ext):
     folder = 'image'
     h = hashlib.sha256()
     block = 128*1024
+    s = BytesIO()
+    res = {}
     try:
         with tempfile.NamedTemporaryFile(delete=False) as f:
             temp_fname = f.name
@@ -40,6 +43,10 @@ def write_file(flow_item, ext):
                 for b in chunks(flow_item.response.content, block):
                     h.update(b)
                     f.write(b)
+                    s.write(b)
+                img = Image.open(s)
+            s.seek(0, os.SEEK_END)
+            filesize = s.tell()
             sha256_csum = h.hexdigest()
             new_bname = '{}.{}'.format(sha256_csum, ext)
             parent_folder = os.path.join(folder, sha256_csum[:2])
@@ -47,27 +54,19 @@ def write_file(flow_item, ext):
             pathlib.Path(parent_folder).mkdir(parents=True, exist_ok=True)
             shutil.move(temp_fname, new_fname)
         ctx.log.info('DONE:{}'.format(new_fname))
+        res = {
+            'value': sha256_csum,
+            'filesize': filesize,
+            'ext': ext,
+            'width': img.size[0],
+            'height': img.size[1],
+            'img_format': img.format,
+            'img_mode': img.mode
+        }
     except Exception as e:
         ctx.log.error('url: {}'.format(flow_item.request.pretty_url))
         ctx.log.error('{}:{}'.format(type(e), e))
-
-
-def response(flow: http.HTTPFlow) -> None:
-    if 'content-type' in flow.response.headers:
-        content_type = flow.response.headers['content-type']
-        if content_type.startswith('image'):
-            # check in database
-            in_databse = False
-            if not in_databse:
-                ext = content_type.split('/')[1].split(';')[0]
-                invalid_exts = ['svg+xml', 'x-icon', 'gif']
-                if ext not in invalid_exts:
-                    write_file(flow, ext)
-
-                    # send to server
-                    req_url = flow.request.pretty_url  # NOQA
-                    #  sha256_csum
-                    #  ext
+    return res
 
 
 # MODEL
@@ -94,6 +93,8 @@ class Sha256Checksum(Base):
 
 class Url(Base):
     value = db.Column(URLType, unique=True, nullable=False)
+    sha256_checksum_id = db.Column(
+        db.Integer, db.ForeignKey('sha256_checksum.id'))
 
 
 # FLASK
@@ -116,7 +117,7 @@ def create_app(script_info=None):
     def shell_context():
         return {
             'app': app, 'db': db, 'session': db.session,
-            'Sha256Checksum': Sha256Checksum, 'Url': 'Url'
+            'Sha256Checksum': Sha256Checksum, 'Url': Url,
         }
     return app
 
@@ -131,6 +132,36 @@ def cli():
 def echo_path():
     'Echo path to script.'
     print(__file__)
+
+
+def save_info(info, url):
+    # TODO
+    pass
+
+
+class ImageProxy:
+
+    def __init___(self):
+        self.app = create_app()
+
+    def response(self, flow: http.HTTPFlow) -> None:
+        if 'content-type' in flow.response.headers:
+            content_type = flow.response.headers['content-type']
+            if content_type.startswith('image'):
+                # check in database
+                url = flow.request.pretty_url
+                in_databse = Url.query.filter_by(value=url).first()
+                if not in_databse:
+                    ext = content_type.split('/')[1].split(';')[0]
+                    invalid_exts = ['svg+xml', 'x-icon', 'gif']
+                    if ext not in invalid_exts:
+                        info = process_flow(flow, ext)
+                        save_info(info, url)
+
+
+addons = [
+    ImageProxy()
+]
 
 
 if __name__ == '__main__':
