966339d99f4ebd76d3ae008f07627a4b8474793c
rachmadaniHaryono <foreturiga@gmail.com>
2020-10-15 13:40:46 +0800

    chg: dev: remove unused code

--- mitmproxy_image/__main__.py
+++ mitmproxy_image/__main__.py
@@ -10,32 +10,17 @@ import logging
 import os
 import pathlib
 import shlex
-import shutil
-import sys
-import tempfile
-import traceback
-from datetime import date, datetime
-from typing import Any, Optional, Tuple, TypeVar, Union
+from datetime import datetime
+from typing import Optional, Union
 
 import click
 import yaml
 from appdirs import user_data_dir
-from flask import Flask, abort, current_app, jsonify
-from flask import request as flask_request
-from flask import send_from_directory, url_for
+from flask import Flask
 from flask.cli import FlaskGroup
-from flask.views import MethodView
 from flask_admin import Admin, AdminIndexView
-from flask_sqlalchemy import SQLAlchemy
-from hashfile import hash_file
 from mitmproxy.tools._main import mitmproxy
-from PIL import Image
-from sqlalchemy.exc import OperationalError
-from sqlalchemy.orm.scoping import scoped_session
 from sqlalchemy.sql import func  # type: ignore  # NOQA
-from sqlalchemy.types import TIMESTAMP
-from sqlalchemy_utils import database_exists
-from sqlalchemy_utils.types import URLType
 
 # app dir
 APP_DIR = user_data_dir('mitmproxy_image', 'rachmadani haryono')
@@ -54,10 +39,6 @@ FORMATTER = logging.Formatter(LOG_FORMAT)
 # db
 DB_PATH = os.path.abspath(os.path.join(APP_DIR, 'mitmproxy_image.db'))
 DB_URI = 'sqlite:///{}'.format(DB_PATH)
-DB = SQLAlchemy()
-# annotation
-Sha256ChecksumVar = TypeVar('Sha256ChecksumVar', bound='Sha256Checksum')
-UrlVar = TypeVar('UrlVar', bound='Url')
 # etc
 KNOWN_IMAGE_EXTS = (
     'gif', 'jpeg', 'jpg', 'png', 'svg+xml', 'vnd.microsoft.icon',
@@ -66,149 +47,9 @@ KNOWN_CONTENT_TYPES = tuple('image/{}'.format(x) for x in KNOWN_IMAGE_EXTS)
 INVALID_IMAGE_EXTS = ['svg+xml', 'x-icon', 'gif', 'vnd.microsoft.icon', 'cur']
 CACHE_SIZE = 1024
 
-
 # MODEL
 
-class BaseModel(DB.Model):  # type: ignore
-    __abstract__ = True
-    id = DB.Column(DB.Integer, primary_key=True)
-    created_at = DB.Column(
-        TIMESTAMP, default=datetime.now, nullable=False)
 
-
-class Sha256Checksum(BaseModel):
-    __tablename__ = 'sha256_checksum'
-    value = DB.Column(DB.String, unique=True)
-    ext = DB.Column(DB.String)
-    filesize = DB.Column(DB.Integer)
-    height = DB.Column(DB.Integer)
-    width = DB.Column(DB.Integer)
-    img_format = DB.Column(DB.String)
-    img_mode = DB.Column(DB.String)
-    urls = DB.relationship('Url', lazy='subquery', backref='checksum')
-    trash = DB.Column(DB.Boolean, default=False)
-
-    def __repr__(self):
-        templ = "<Sha256Checksum(id={}, value={}...)>"
-        return templ.format(self.id, self.value[:7])
-
-    def to_dict(self, include_urls=True):
-        res = {}
-        keys = [
-            'value', 'ext', 'filesize', 'height', 'width', 'img_format',
-            'img_mode', 'trash']
-        if include_urls:
-            keys.append('urls')
-        for k in keys:
-            if k == 'urls':
-                res[k] = [x.to_dict(
-                    include_checksum=False) for x in getattr(self, k, [])]
-            else:
-                res[k] = getattr(self, k)
-        return res
-
-    @staticmethod
-    def get_or_create(
-            filepath: str,
-            url: Optional[Union[str, UrlVar]] = None,
-            session: Optional[scoped_session] = None,
-            image_dir: Union[str, 'os.PathLike[str]'] = IMAGE_DIR
-    ) -> Tuple[Sha256ChecksumVar, bool]:
-        hash_value = hash_file(filepath, 'sha256')
-        instance, created = get_or_create(
-            session, Sha256Checksum, value=hash_value)
-        if url and isinstance(url, str):
-            url_m = get_or_create(session, Url, value=url)[0]
-            instance.urls.append(url_m)
-        elif url and isinstance(url, Url):
-            url_m = url
-            instance.urls.append(url_m)
-        if created:
-            instance.filesize = os.path.getsize(filepath)
-            pil_img = Image.open(filepath)
-            instance.ext = pil_img.format.lower()
-            instance.width, instance.height = pil_img.size
-            instance.img_format = pil_img.format
-            instance.img_mode = pil_img.mode
-            instance.trash = False
-        if not instance.trash:
-            new_filepath = os.path.join(
-                image_dir, hash_value[:2],
-                hash_value + '.{}'.format(instance.ext))
-            pathlib.Path(os.path.dirname(
-                new_filepath)).mkdir(parents=True, exist_ok=True)
-            if filepath != new_filepath:
-                shutil.copyfile(filepath, new_filepath)
-            else:
-                logging.debug('Same file: {}'.format(filepath))
-        return instance, created
-
-
-class Url(BaseModel):
-    __tablename__ = 'url'
-    value = DB.Column(URLType, unique=True, nullable=False)
-    sha256_checksum_id = DB.Column(
-        DB.Integer, DB.ForeignKey('sha256_checksum.id'))
-    redirect_counter = DB.Column(DB.Integer, default=0)
-    check_counter = DB.Column(DB.Integer, default=0)
-    last_redirect = DB.Column(
-        DB.DateTime, server_default=func.now())
-    last_check = DB.Column(
-        DB.DateTime, server_default=func.now())
-
-    def __repr__(self):
-        templ = "<Url(id={}, value={}, checksum={})>"
-        return templ.format(self.id, self.value, self.checksum)
-
-    def to_dict(self, include_checksum=True):
-        res = {}
-        keys = [
-            'id', 'value', 'redirect_counter',  'check_counter',
-            'last_redirect', 'last_check']
-        if include_checksum:
-            keys.append('checksum')
-        for k in keys:
-            if k == 'checksum' and self.checksum:
-                res[k] = self.checksum.to_dict(include_urls=False)
-            if k == 'checksum':
-                pass
-            else:
-                res[k] = str(getattr(self, k))
-        if self.checksum:
-            try:
-                img_url = url_for(
-                    '.image_url', _external=True, filename='{}.{}'.format(
-                        self.checksum.value, self.checksum.ext)),
-                res['img_url'] = img_url
-            except RuntimeError as err:
-                logging.warning('{}:{}'.format(type(err), err))
-        return res
-
-    @staticmethod
-    def get_or_create(
-            value: str,
-            session: Optional[scoped_session] = DB.session
-    ) -> Tuple[UrlVar, bool]:
-        instance, created = get_or_create(session, Url, value=value)
-        return instance, created
-
-
-def get_or_create(
-        session: scoped_session,
-        model: Any,
-        **kwargs
-) -> Tuple[Any, bool]:
-    """Creates an object or returns the object if exists."""
-    instance = session.query(model).filter_by(**kwargs).first()
-    created = False
-    if not instance:
-        instance = model(**kwargs)
-        session.add(instance)
-        created = True
-    return instance, created
-
-
-# FLASK
 def create_app(
         db_uri=DB_URI,
         debug: Optional[bool] = False,
@@ -244,34 +85,13 @@ def create_app(
     for folder in folders:
         pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
     # db
-    DB.init_app(app)
     app.app_context().push()
-    if \
-            not database_exists(db_uri) or \
-            not DB.engine.dialect.has_table(DB.engine, 'url'):
-        DB.create_all()
-        logging.debug('database created')
 
     @app.shell_context_processor
     def shell_context():
-        return {
-            'app': app, 'DB': DB,
-            'Sha256Checksum': Sha256Checksum, 'Url': Url,
-        }
+        return {'app': app, }
 
     # route
-    sha256_checksum_view = Sha256ChecksumView.as_view('sha256_checksum')
-    app.add_url_rule(
-        '/api/sha256_checksum',
-        view_func=sha256_checksum_view,
-        methods=['GET', 'POST'])
-    url_view = UrlView.as_view('url_view')
-    app.add_url_rule(
-        '/api/url',
-        view_func=url_view,
-        methods=['GET', 'POST'])
-    app.add_url_rule('/i/<path:filename>', 'image_url', image_url)
-
     def test():
         app.logger.debug('test page')
         return 'hello world'
@@ -284,268 +104,10 @@ def create_app(
     return app
 
 
-class Sha256ChecksumView(MethodView):
-
-    def get(self):
-        db_session = DB.session
-        input_query = flask_request.args.get('q')
-        qs_dict = {}
-        if input_query is not None:
-            for item in input_query.split():
-                if ':' not in item:
-                    continue
-                parts = item.split(':', 1)
-                qs_dict[parts[0]] = parts[1]
-        #  initial value
-        per_page = 200
-        created_at = None
-        # per_page
-        per_page = int(os.environ.get('MITMPROXY_IMAGE_PER_PAGE', per_page))
-        per_page = int(flask_request.args.get('per_page', per_page))
-        per_page = int(qs_dict.get('per_page', per_page))
-        # created_at
-        created_at = flask_request.args.get('created_at', created_at)
-        created_at = qs_dict.get('created_at', created_at)
-        # other args
-        page = int(flask_request.args.get('page', 1))
-        res = {}
-        try:
-            dsq = db_session.query(Sha256Checksum) \
-                .filter_by(trash=False) \
-                .order_by(Sha256Checksum.created_at.desc())
-            if created_at is not None and created_at == 'today':
-                dsq = dsq.filter(
-                    func.DATE(Sha256Checksum.created_at) == date.today()
-                )
-            if per_page > 0:
-                dsq = dsq.limit(per_page).offset((int(page) - 1) * per_page)
-            items = dsq.all()
-            res = {
-                'items': [{
-                    'created_at': str(item.created_at),
-                    'value': str(item.value),
-                    'urls': [url.value for url in item.urls],
-                    'img_url': url_for(
-                        '.image_url', _external=True,
-                        filename='{}.{}'.format(item.value, item.ext)),
-                } for item in items],
-                'next_page': url_for(
-                    'sha256_checksum', page=page+1, q=input_query,
-                    _external=True)
-            }
-        finally:
-            db_session.remove()
-        return jsonify(res)
-
-    def post(self):
-        db_session = DB.session
-        # check if the post request has the file part
-        if 'file' not in flask_request.files:
-            return jsonify({'error': 'No file part'})
-        file_ = flask_request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file_.filename == '':
-            return jsonify({'error': 'No selected file'})
-        with tempfile.NamedTemporaryFile(delete=False) as f_temp:
-            file_.save(f_temp.name)
-            url = flask_request.form.get('url', None)
-            url_m = None
-            try:
-                if url is not None:
-                    url_m, _ = get_or_create(db_session, Url, value=url)
-                # TODO
-                raise NotImplementedError
-                with db_session.no_autoflush:
-                    checksum_m = Sha256Checksum.get_or_create(
-                        f_temp.name, session=db_session)[0]
-                if url_m is not None:
-                    checksum_m.urls.append(url_m)
-                db_session.add(checksum_m)
-                db_session.commit()
-                current_app.logger.debug(
-                    'SERVER POST:\nurl: {}\nchecksum: {}'.format(
-                        url_m.value, checksum_m.value)
-                )
-            except OperationalError as err:
-                db_session.rollback()
-                current_app.logger.error(traceback.format_exc())
-                current_app.logger.error('{}:{}'.format(
-                    type(err), err))
-                abort(500)
-                return
-            finally:
-                db_session.remove()
-        return jsonify({'status': 'success'})
-
-
-class UrlView(MethodView):
-
-    def get(self):
-        db_session = DB.session
-        url_value = flask_request.args.get('value', None)
-        if url_value is None:
-            return jsonify([x.to_dict() for x in db_session.query(Url).all()])
-        res = {}
-        try:
-            url_m = \
-                db_session.query(Url).filter_by(value=url_value).one_or_none()
-            if url_m is None:
-                abort(404)
-                return
-            res = url_m.to_dict()
-            if url_m.checksum:
-                res['checksum_value'] = url_m.checksum_value
-                res['checksum_trash'] = url_m.checksum_trash
-        except OperationalError as err:
-            current_app.logger.error(traceback.format_exc())
-            current_app.logger.error('{}:{}\n{}:{}'.format(
-                type(err), err, 'URL', url_value))
-            res['error'] = str(err)
-            db_session.rollback()
-        finally:
-            db_session.close()
-        return jsonify(res)
-
-    def post(self):
-        db_session = DB.session
-        url_value = flask_request.form.get('value', None)
-        if url_value is None:
-            abort(404)
-            return
-        res = {}
-        try:
-            url_m = \
-                db_session.query(Url).filter_by(value=url_value).one_or_none()
-            if url_m is None:
-                url_m = \
-                    Url.get_or_create(value=url_value, session=db_session)[0]
-                db_session.commit()
-            res = url_m.to_dict()
-            if url_m.checksum:
-                res['checksum_value'] = url_m.checksum_value
-                res['checksum_trash'] = url_m.checksum_trash
-            for key in ('redirect_counter''check_counter'):
-                var = flask_request.form.get(key, None)
-                if var and var == '+1':
-                    if getattr(url_m, key, None) is None:
-                        setattr(url_m, key)
-                    else:
-                        setattr(url_m, key, getattr(url_m, key) + 1)
-                elif var:
-                    current_app.logger.error('Unknown input:{}:{}'.format(
-                        key, var))
-            db_session.add(url_m)
-            db_session.commit()
-            res['redirect_counter'] = url_m.redirect_counter
-            res['check_counter'] = url_m.check_counter
-        except OperationalError as err:
-            current_app.logger.error(traceback.format_exc())
-            current_app.logger.error('{}:{}\n{}:{}'.format(
-                type(err), err, 'URL', url_value))
-            res['error'] = str(err)
-            db_session.rollback()
-        finally:
-            db_session.close()
-        return jsonify(res)
-
-
-def image_url(filename):
-    return send_from_directory(
-        IMAGE_DIR, os.path.join(filename[:2], filename))
-
-
 @click.group(cls=FlaskGroup, create_app=create_app)
 def cli():
     """This is a script for application."""
     pass
-
-
-@cli.command('echo-path')
-def echo_path():
-    'Echo path to script.'
-    print(__file__)
-
-
-def scan_image_folder():
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    im_data = []
-    label = 'Getting all files'
-    with click.progressbar(os.walk(IMAGE_DIR), label=label) as bar:
-        for root, _, files in bar:
-            for ff in files:
-                im_data.append({
-                    'basename': ff,
-                    'value': os.path.splitext(os.path.basename(ff))[0],
-                    'ext': os.path.splitext(ff)[1][1:],
-                })
-    db_session = DB.session
-
-    if not im_data:
-        mappings = []
-        i = 0
-        q_ = db_session.query(Sha256Checksum).filter_by(trash=False)
-        non_trash_count = q_.count()
-        for m in q_.all():
-            info = {'id': m.id, 'trash': True}
-            mappings.append(info)
-            i = i + 1
-            if i % 10000 == 0:
-                db_session.bulk_update_mappings(Sha256Checksum, mappings)
-                db_session.flush()
-                db_session.commit()
-                mappings[:] = []
-        db_session.bulk_update_mappings(Sha256Checksum, mappings)
-        print(
-            '[non trash]before:{}, after:{}'.format(
-                non_trash_count,
-                db_session.query(Sha256Checksum)
-                .filter_by(trash=False).count()))
-        db_session.commit()
-    else:
-        existing_items, missing_items = [], []
-        q_ = db_session.query(Sha256Checksum).filter_by(trash=False)
-        db_items = q_.all()
-        label = 'Differentiate missing/existing items'
-        with click.progressbar(im_data, label=label) as pg_im_data:
-            for x in pg_im_data:
-                (missing_items, existing_items)[
-                    any(getattr(d, 'value') == x['value'] for d in db_items)
-                ].append(x)
-        if q_.count() != len(existing_items):
-            # TODO set non non existing_items to trash
-            raise NotImplementedError
-        csm_ms = []
-        if not missing_items:
-            print('All item are on database.')
-            return
-        label = 'Processing missing items'
-        with click.progressbar(missing_items, label=label) as pg_missing_items:
-            for item in pg_missing_items:
-                file_path = os.path.join(
-                    IMAGE_DIR, item['value'][:2], item['basename'])
-                checksum_m = Sha256Checksum.get_or_create(
-                    file_path, session=db_session)[0]
-                #  if info['value'] != item['value']:  # TODO
-                checksum_m.trash = False
-                csm_ms.append(checksum_m)
-        db_session.add_all(csm_ms)
-        db_session.commit()
-
-
-@cli.command('scan-image-folder')
-def scan_image_folder_command():
-    """Scan image folder.
-
-    This function find problems in image folder.
-    - empty filesize (trash)
-    - empty ext (process info, save if image)
-    - file not in database (process info, save  if image)
-    - file in database but not in image folder (set trash prop = True)
-    - file in db and folder, trash prop=true (set trash prop = False)
-    - file in database but empty filesize (trash)
-    """
-    scan_image_folder()
 
 
 @cli.command('run-mitmproxy')
--- tests/test_mitmproxy_image.py
+++ tests/test_mitmproxy_image.py
@@ -4,10 +4,8 @@ import unittest
 from unittest import mock
 
 import pytest
-from PIL import Image
-from sqlalchemy_utils import database_exists
 
-from mitmproxy_image.__main__ import DB, Sha256Checksum, Url, create_app
+from mitmproxy_image.__main__ import create_app
 from mitmproxy_image.script import MitmImage
 
 PICKLE_PATH = os.path.join(
@@ -27,94 +25,9 @@ class Mitmproxy_imageTestCase(unittest.TestCase):
         rv = self.app.get('/')
         self.assertIn('Mitmproxy-Image', rv.data.decode())
 
-    def test_url_list(self):
-        url_value = 'http://example.com'
-        assert DB.engine.dialect.has_table(DB.engine, 'url')
-        rv = self.app.get('/api/url')
-        jv = rv.get_json()
-        assert not jv
-        #  rv = self.app.get('/api/url', data={'value': url_value})
-        #  assert rv.status_code == 404
-        rv = self.app.post('/api/url', data={'value': url_value})
-        jv = rv.get_json()
-        jv.pop('last_check')
-        jv.pop('last_redirect')
-        assert jv == {
-            'check_counter': 0,
-            'id': '1',
-            'redirect_counter': 0,
-            'value': 'http://example.com'}
 
-
-def test_get_or_create_url_model():
-    db_uri = 'sqlite://'
-    app = create_app(db_uri=db_uri, debug=True, testing=True)
-    url_value = 'http://example.com/1.html'
-    session = DB.session
-    with app.app_context():
-        m, created = Url.get_or_create(url_value, session)
-        session.commit()
-        assert created
-        m_dict = m.to_dict()
-        m_dict.pop('last_redirect')
-        m_dict.pop('last_check')
-        m_dict.pop('id')
-        assert m_dict == {
-            'check_counter': '0',
-            'redirect_counter': '0',
-            'value': url_value
-        }
-
-
-def test_get_or_create_sha256checksum_model(tmp_path):
-    app = create_app(db_uri='sqlite://', debug=True, testing=True)
-    url = 'http://example.com/1.jpeg'
-    session = DB.session
-    img = Image.new('RGB', (60, 30), color='red')
-    test_img = tmp_path / 'test.jpg'
-    test_dir = tmp_path / 'test_dir'
-    test_dir.mkdir()
-    img.save(test_img)
-    hash_ = '71bfa8254d2cbdbdfe56938cdbf0c759be4d3d80818b56652de89fc589a70cbe'
-    with app.app_context():
-        m, created = Sha256Checksum.get_or_create(
-            test_img.as_posix(), url, session, tmp_path.as_posix())
-        session.commit()
-        assert created
-        m_dict = m.to_dict()
-        m_dict['urls'][0].pop('last_check')
-        m_dict['urls'][0].pop('last_redirect')
-        m_dict['urls'][0].pop('redirect_counter')
-        assert m_dict == {
-            'ext': 'jpeg',
-            'filesize': 661,
-            'height': 30,
-            'img_format': 'JPEG',
-            'img_mode': 'RGB',
-            'trash': False,
-            'urls': [{
-                'check_counter': '0',
-                'id': '1',
-                'value': 'http://example.com/1.jpeg'}],
-            'value': hash_,
-            'width': 60}
-
-
-@pytest.mark.parametrize('use_file', [True, False])
-def test_create_app(tmp_path, use_file):
-    if not use_file:
-        assert create_app('sqlite://')
-    else:
-        test_file = tmp_path / 'test.db'
-        db_uri = 'sqlite:///{}'.format(test_file.as_posix())
-        #  before create app
-        assert not test_file.is_file()
-        assert not database_exists(db_uri)
-        # create app
-        assert create_app(db_uri)
-        # after create app
-        assert test_file.is_file()
-        assert database_exists(db_uri)
+def test_create_app():
+    assert create_app()
 
 
 def test_mitmimage_init():
