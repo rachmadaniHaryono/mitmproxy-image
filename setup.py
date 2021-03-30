8b19d30691a063dfff870ec696e99c8b8fed4a2e
rachmadaniHaryono <foreturiga@gmail.com>
2021-03-30 09:46:05 +0800

    chg: dev: more-itertools minimum requirement

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         "hydrus-api>=2.14.3",
         "ipdb>=0.13.3",
         "mitmproxy>=6.0.0",
-        "more-itertools==8.7.0",
+        "more-itertools>=8.7.0",
         "Pillow>=5.3.0",
         "python-json-logger>=2.0.1",
         "python-magic>=0.4.22",
